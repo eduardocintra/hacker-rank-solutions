97521a0fb53d9b79b501ee906d9e67b46bcd3779
eduardocintra <eduardocintramaia@gmail.com>
2024-07-13 19:44:07 -0300

    feat: Electronics Shop [Easy] Solution

---
+++ src/br/com/eduardocintra/easy/electronicsshop/ElectronicsShop.java
@@ -0,0 +1,38 @@
+package br.com.eduardocintra.easy.electronicsshop;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ElectronicsShop {
+
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+        /*
+         * Write your code here.
+         */
+        int moneySpent = -1;
+        for (int keyBoardPrice : keyboards) {
+            for (int driverPrice : drives) {
+                int sum = keyBoardPrice + driverPrice;
+                if (sum <= b && sum > moneySpent) {
+                    moneySpent = sum;
+                }
+            }
+        }
+
+        return moneySpent;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int[] firstLine = Arrays.stream(sc.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+        int[] keyboards = Arrays.stream(sc.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+        int[] drives = Arrays.stream(sc.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+        int b = firstLine[0];
+        System.out.println(getMoneySpent(keyboards, drives, b));
+    }
+}
