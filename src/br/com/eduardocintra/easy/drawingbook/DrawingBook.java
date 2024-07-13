3de2917e0bbc50e9837b887d5f1b123423dcf592
eduardocintra <eduardocintramaia@gmail.com>
2024-07-13 15:22:03 -0300

    feat: Drawing Book Solution

---
+++ src/br/com/eduardocintra/easy/drawingbook/DrawingBook.java
@@ -0,0 +1,35 @@
+package br.com.eduardocintra.easy.drawingbook;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        // Fix number of page when is an even number of pages
+        if(n % 2 == 0) n += 1;
+        int costStartToEnd = p/2;
+        int costEndToStart = (n-p)/2;
+        return Math.min(costStartToEnd, costEndToStart);
+    }
+
+}
+
+public class DrawingBook {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int p = Integer.parseInt(sc.nextLine());
+        System.out.println(Result.pageCount(n, p));
+    }
+}
