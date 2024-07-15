f5d3c9a22db1954c8d7b7ba47cb9fa4642ea712b
eduardocintra <eduardocintramaia@gmail.com>
2024-07-14 22:05:52 -0300

    feat: Picking Numbers [Easy] Solution

---
+++ src/br/com/eduardocintra/easy/pickingnumbers/PickingNumbers.java
@@ -0,0 +1,66 @@
+package br.com.eduardocintra.easy.pickingnumbers;
+
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+        Collections.sort(a);
+        List<Integer> sub = new ArrayList<>();
+        int i = 0;
+        int maxSize = 0;
+        while(i < a.size()) {
+
+            int number = a.get(i);
+
+            if(isValidNumber(sub, number)) {
+                sub.add(number);
+                i++;
+            } else {
+                maxSize = Math.max(maxSize, sub.size());
+                sub = new ArrayList<>();
+            }
+
+            maxSize = Math.max(maxSize, sub.size());
+        }
+        return maxSize;
+    }
+
+    private static boolean isValidNumber(List<Integer> subList, int numberTest) {
+        for(int number : subList) {
+            if(Math.abs(number-numberTest) > 1) {
+                return  false;
+            }
+        }
+        return true;
+    }
+
+}
+
+public class PickingNumbers {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int size = Integer.parseInt(sc.nextLine());
+        List<Integer> a = Arrays.stream(sc.nextLine().split("\\s")).map(Integer::parseInt).collect(toList());
+        if(size != a.size()) {
+            throw new InvalidParameterException("The size of array is invalid");
+        }
+        System.out.println(Result.pickingNumbers(a));
+    }
+}
