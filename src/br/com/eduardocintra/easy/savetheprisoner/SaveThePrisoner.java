140d1552c06ec23b16e9f4456c9e7d6f3c74ca34
eduardocintra <eduardocintramaia@gmail.com>
2024-07-20 00:30:52 -0300

    feat: Save the Prisoner! Solution [Easy]

---
+++ src/br/com/eduardocintra/easy/savetheprisoner/SaveThePrisoner.java
@@ -0,0 +1,59 @@
+package br.com.eduardocintra.easy.savetheprisoner;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'saveThePrisoner' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     *  3. INTEGER s
+     */
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        // Write your code here
+        int lastChair = (s + m - 1) % n;
+        return lastChair == 0 ? n : lastChair;
+    }
+
+}
+
+public class SaveThePrisoner {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.saveThePrisoner(n, m, s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
