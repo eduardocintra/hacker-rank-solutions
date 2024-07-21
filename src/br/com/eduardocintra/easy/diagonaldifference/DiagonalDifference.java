43dcc3fc91513d92876aa2e4ff7495dacf751026
eduardocintra <eduardocintramaia@gmail.com>
2024-07-20 22:58:40 -0300

    fix: input stream

--- src/br/com/eduardocintra/easy/diagonaldifference/DiagonalDifference.java
+++ src/br/com/eduardocintra/easy/diagonaldifference/DiagonalDifference.java
@@ -1,16 +1,10 @@
 package br.com.eduardocintra.easy.diagonaldifference;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
 class Result {
 
@@ -22,20 +16,8 @@ class Result {
    */
 
   public static int diagonalDifference(List<List<Integer>> arr) {
-
     int leftToRighDiagonalSum = 0;
     int rightToLeftDiagonalSum = 0;
-    //        for(int l = 0; l < arr.size(); l++) {
-    //            for(int c = 0; c < arr.get(0).size(); c++) {
-    //                if(l == c) {
-    //                    leftToRighDiagonalSum += arr.get(l).get(c);
-    //                }
-    //                if(c == (arr.get(l).size() - (l + 1))) {
-    //                    rightToLeftDiagonalSum += arr.get(l).get(c);
-    //                }
-    //            }
-    //        }
-    //
 
     // Improved version reducing algorithm complexity
     for (int i = 0; i < arr.size(); i++) {
@@ -48,13 +30,32 @@ class Result {
 
 public class DiagonalDifference {
   public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    List<Integer> l1 = Arrays.asList(11, 2, 4);
-    List<Integer> l2 = Arrays.asList(4, 5, 6);
-    List<Integer> l3 = Arrays.asList(10, 8, -12);
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-    List<List<Integer>> list = Arrays.asList(l1, l2, l3);
+    List<List<Integer>> arr = new ArrayList<>();
 
-    System.out.println(Result.diagonalDifference(list));
+    IntStream.range(0, n)
+        .forEach(
+            i -> {
+              try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList()));
+              } catch (IOException ex) {
+                throw new RuntimeException(ex);
+              }
+            });
+
+    int result = Result.diagonalDifference(arr);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
   }
 }
