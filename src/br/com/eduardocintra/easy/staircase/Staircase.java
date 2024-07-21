1e723a8528e9ceb32346cfa57bf6719159cf9d5b
eduardocintra <eduardocintramaia@gmail.com>
2024-07-20 23:04:59 -0300

    fix: input stream

--- src/br/com/eduardocintra/easy/staircase/Staircase.java
+++ src/br/com/eduardocintra/easy/staircase/Staircase.java
@@ -1,16 +1,6 @@
 package br.com.eduardocintra.easy.staircase;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 class Result {
 
@@ -19,10 +9,11 @@ class Result {
    *
    * The function accepts INTEGER n as parameter.
    */
+
   public static void staircase(int n) {
     for (int i = 1; i <= n; i++) {
-      for (int k = 0; k < i; k++) {
-        if (k == 0) printSpaces(n - i);
+      for(int k = 0;  k <i; k++) {
+        if(k == 0) printSpaces(n-i);
         System.out.print("#");
       }
       System.out.println();
@@ -30,16 +21,20 @@ class Result {
   }
 
   private static void printSpaces(int n) {
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
       System.out.print(" ");
     }
   }
 }
 
 public class Staircase {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int size = Integer.parseInt(sc.nextLine());
-    Result.staircase(size);
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    Result.staircase(n);
+
+    bufferedReader.close();
   }
 }
