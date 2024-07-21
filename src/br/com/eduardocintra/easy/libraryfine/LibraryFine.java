bd2983cbb3a1e4248b75af3990e3a0340c2048da
eduardocintra <eduardocintramaia@gmail.com>
2024-07-21 18:02:18 -0300

    feat: Library Fine Solution [Easy]

---
+++ src/br/com/eduardocintra/easy/libraryfine/LibraryFine.java
@@ -0,0 +1,63 @@
+package br.com.eduardocintra.easy.libraryfine;
+
+import java.io.*;
+
+class Result {
+
+  /*
+   * Complete the 'libraryFine' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts following parameters:
+   *  1. INTEGER d1
+   *  2. INTEGER m1
+   *  3. INTEGER y1
+   *  4. INTEGER d2
+   *  5. INTEGER m2
+   *  6. INTEGER y2
+   */
+  public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+    // Write your code here
+    if (y1 > y2) {
+      return 10000;
+    }
+    if (m1 > m2 && y1 == y2) {
+      return (m1 - m2) * 500;
+    }
+    if (d1 > d2 && m1 == m2 && y1 == y2) {
+      return (d1 - d2) * 15;
+    }
+    return 0;
+  }
+}
+
+public class LibraryFine {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+    int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+    int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+    int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+    String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+    int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+    int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+    int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+    int result = Result.libraryFine(d1, m1, y1, d2, m2, y2);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
