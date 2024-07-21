ba03fdb49386716b85c5c2dd92040e2c94ab0347
eduardocintra <eduardocintramaia@gmail.com>
2024-07-20 23:13:01 -0300

    fix: input stream

--- src/br/com/eduardocintra/easy/timeconversion/TimeConversion.java
+++ src/br/com/eduardocintra/easy/timeconversion/TimeConversion.java
@@ -1,7 +1,8 @@
 package br.com.eduardocintra.easy.timeconversion;
 
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
 
 class Result {
 
@@ -11,23 +12,30 @@ class Result {
    * The function is expected to return a STRING.
    * The function accepts STRING s as parameter.
    */
-
   public static String timeConversion(String s) {
     String format = s.replaceAll("[^APM]", "");
-    String timeWithoutFormat = s.replaceAll("[APM]", "");
-    String[] aTime = timeWithoutFormat.split(":");
+    String sWithoutFormat = s.replaceAll("[APM]", "");
+    String[] aTime = sWithoutFormat.split(":");
     int hour = Integer.parseInt(aTime[0]);
     if (format.equalsIgnoreCase("PM") && hour != 12) hour += 12;
     if (format.equalsIgnoreCase("AM") && hour == 12) hour = 0;
-
     return String.format("%02d", hour) + ":" + aTime[1] + ":" + aTime[2];
   }
 }
 
 public class TimeConversion {
   public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-    String input = sc.nextLine();
-    System.out.println(Result.timeConversion(input));
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String s = bufferedReader.readLine();
+
+    String result = Result.timeConversion(s);
+
+    bufferedWriter.write(result);
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
   }
 }
