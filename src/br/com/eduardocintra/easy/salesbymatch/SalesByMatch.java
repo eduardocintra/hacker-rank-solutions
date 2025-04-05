d6dd18e3abe6f52d01908c26a3864c126a82214c
eduardocintra <eduardocintramaia@gmail.com>
2025-04-05 14:19:16 -0300

    fix: main function

--- src/br/com/eduardocintra/easy/salesbymatch/SalesByMatch.java
+++ src/br/com/eduardocintra/easy/salesbymatch/SalesByMatch.java
@@ -1,10 +1,12 @@
 package br.com.eduardocintra.easy.salesbymatch;
 
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 class Result {
 
@@ -38,6 +40,22 @@ class Result {
 
 public class SalesByMatch {
   public static void main(String[] args) throws IOException {
-    System.out.println(Result.sockMerchant(7, Arrays.asList(1, 2, 1, 2, 1, 3, 2)));
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+    int result = Result.sockMerchant(n, ar);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
   }
 }
+
